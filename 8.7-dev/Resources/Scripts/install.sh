0355c5083eba1a0735968e4af55f132899d35a0c
Andy Hausmann <hi@andy-hausmann.de>
2018-07-02 23:25:34 +0200

    [TASK] Improve docker image and ad compose file

--- 8.7-dev/Resources/Scripts/install.sh
+++ 8.7-dev/Resources/Scripts/install.sh
@@ -2,15 +2,16 @@
 
 cd /var/www/html && \
     ./typo3conf/ext/typo3_console/typo3cms install:setup --force \
-        --database-user-name "typo3" --database-user-password "typo3pwd" \
-        --database-host-name "t38dev-db" --database-name "typo3" --database-port "3306" --database-socket "" \
+        --database-user-name "user" --database-user-password "pass" \
+        --database-host-name "db" --database-name "typo3" --database-port "3306" --database-socket "" \
         --admin-user-name "admin" --admin-password "password" \
         --site-name "TYPO3 Project" --site-setup-type "no" \
         --no-interaction 1 --use-existing-database 1 && \
-    ./typo3conf/ext/typo3_console/typo3cms cache:flush && \
+    ./typo3conf/ext/typo3_console/typo3cms cache:flushcomplete && \
     ./typo3/cli_dispatch.phpsh extbase extension:install scheduler && \
     ./typo3/cli_dispatch.phpsh extbase extension:install typo3_console && \
     ./typo3/cli_dispatch.phpsh extbase extension:install bootstrap_package && \
     ./typo3/cli_dispatch.phpsh extbase extension:install introduction && \
     ./typo3/cli_dispatch.phpsh extbase extension:install extension_builder && \
     chown -R www-data. .
+
